14a450de0e588221560cc753ad8ffcb1e9d88247
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-06-26 17:28:55 -0300

    articles back finish

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -11,9 +11,22 @@ module.exports = app => {
         .get(app.api.category.get)
         .post(app.api.category.save)
 
+    //cuidado com a ordem das rotas
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
+
     app.route('/categories/:id')
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove)
+
+    app.route('/articles')
+        .get(app.api.articles.get)
+        .post(app.api.articles.save)
+
+    app.route('/articles/:id')
+        .get(app.api.articles.getById)
+        .put(app.api.articles.save)
+        .delete(app.api.articles.remove)
 
 }
